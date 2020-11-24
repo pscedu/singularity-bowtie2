e8cf91576bb6a7b5e8effc8820ea5481f8886c43
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2020-11-24 15:00:46 -0500

    Updated scripts for Bridges2

--- 2.2.5/modulefile.lua
+++ 2.2.5/modulefile.lua
@@ -1,29 +1,43 @@
-#%Module1.0#################################################################
-##
-## bowtie2 2.4.2 modulefile
-##
-## "URL: https://www.psc.edu/resources/software"
-## "Category: Biological Sciences"
-## "Description: bowtie2 is an ultrafast and memory-efficient tool for aligning sequencing reads to long reference sequences"
-## "Keywords: singularity bioinformatics"
-#
+--
+-- bowtie2 2.2.5 modulefile
+--
+-- "URL: https://www.psc.edu/resources/software"
+-- "Category: Biological Sciences"
+-- "Description: bowtie2 is an ultrafast and memory-efficient tool for aligning sequencing reads to long reference sequences"
+-- "Keywords: singularity bioinformatics"
 
 whatis("Name: bowtie2")
-whatis("Version: 2.4.2")
+whatis("Version: 2.2.5")
 whatis("Category: Biological Sciences")
 whatis("URL: https://www.psc.edu/resources/software")
 whatis("Description: bowtie2 is an ultrafast and memory-efficient tool for aligning sequencing reads to long reference sequences")
 
 help([[
-bowtie2 2.4.2
+bowtie2 2.2.5
+-------------
 
 Description
+-----------
 bowtie2 is an ultrafast and memory-efficient tool for aligning sequencing reads to long reference sequences. It is particularly good at aligning reads of about 50 up to 100s or 1,000s of characters, and particularly good at aligning to relatively long (e.g. mammalian) genomes. Bowtie 2 indexes the genome with an FM Index to keep its memory footprint small: for the human genome, its memory footprint is typically around 3.2 GB. Bowtie 2 supports gapped, local, and paired-end alignment modes.
 
+To load the module type
+
+> module load bowtie2/2.2.5
+
+To unload the module type
+
+> module unload bowtie2/2.2.5
+
 Documentation
+-------------
 http://bowtie-bio.sourceforge.net/bowtie2/manual.shtml
 
+For help, type
+
+> bowtie2 --help
+
 Repository
+----------
 https://github.com/BenLangmead/bowtie2
 
 Tools included in this module are
@@ -31,7 +45,7 @@ Tools included in this module are
 * bowtie2
 ]])
 
-local package = "bowtie"
-local version = "2.4.2"
+local package = "bowtie2"
+local version = "2.2.5"
 local base    = pathJoin("/opt/packages",package,version)
 prepend_path("PATH", base)
---
+++ 2.4.2/modulefile.lua
@@ -0,0 +1,51 @@
+--
+-- bowtie2 2.4.2 modulefile
+--
+-- "URL: https://www.psc.edu/resources/software"
+-- "Category: Biological Sciences"
+-- "Description: bowtie2 is an ultrafast and memory-efficient tool for aligning sequencing reads to long reference sequences"
+-- "Keywords: singularity bioinformatics"
+
+whatis("Name: bowtie2")
+whatis("Version: 2.4.2")
+whatis("Category: Biological Sciences")
+whatis("URL: https://www.psc.edu/resources/software")
+whatis("Description: bowtie2 is an ultrafast and memory-efficient tool for aligning sequencing reads to long reference sequences")
+
+help([[
+bowtie2 2.4.2
+-------------
+
+Description
+-----------
+bowtie2 is an ultrafast and memory-efficient tool for aligning sequencing reads to long reference sequences. It is particularly good at aligning reads of about 50 up to 100s or 1,000s of characters, and particularly good at aligning to relatively long (e.g. mammalian) genomes. Bowtie 2 indexes the genome with an FM Index to keep its memory footprint small: for the human genome, its memory footprint is typically around 3.2 GB. Bowtie 2 supports gapped, local, and paired-end alignment modes.
+
+To load the module type
+
+> module load bowtie2/2.4.2
+
+To unload the module type
+
+> module unload bowtie2/2.4.2
+
+Documentation
+-------------
+http://bowtie-bio.sourceforge.net/bowtie2/manual.shtml
+
+For help, type
+
+> bowtie2 --help
+
+Repository
+----------
+https://github.com/BenLangmead/bowtie2
+
+Tools included in this module are
+
+* bowtie2
+]])
+
+local package = "bowtie2"
+local version = "2.4.2"
+local base    = pathJoin("/opt/packages",package,version)
+prepend_path("PATH", base)
