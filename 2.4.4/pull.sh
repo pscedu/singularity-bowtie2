7319e04ac50e3ae6ee2e8ed0a0e189a853b372af
halbakri <84340692+halbakri@users.noreply.github.com>
2021-06-28 15:59:38 -0400

    Update pull.sh

--- 2.4.4/pull.sh
+++ 2.4.4/pull.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 singularity pull https://depot.galaxyproject.org/singularity/bowtie2%3A2.4.2--py38h1c8e9b9_0
-mv -v bowtie2%3A2.4.2--py38h1c8e9b9_0 singularity-bowtie2-2.4.2.sif
+mv -v bowtie2%3A2.4.2--py38h1c8e9b9_0 singularity-bowtie2-2.4.4.sif
