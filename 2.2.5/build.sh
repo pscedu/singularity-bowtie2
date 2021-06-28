31f62ee312d36c0eb54051cef9be8cf8333a6730
icaoberg <icaoberg@andrew.cmu.edu>
2021-06-28 15:41:56 -0400

    Update build.sh

--- 2.2.5/build.sh
+++ 2.2.5/build.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+# Copyright © 2021 Pittsburgh Supercomputing Center.
+# All Rights Reserved.
+
+IMAGE=singularity-bowtie2-2.2.5.sif
+DEFINITION=Singularity
+
+if [ -f $IMAGE ]; then
+	rm -fv $IMAGE
+fi
+
+sudo singularity build $IMAGE $DEFINITION
+
+if [ -f $IMAGE ]; then
+	exit 0
+else
